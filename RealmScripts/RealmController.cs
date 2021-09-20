26509a87eab0abf6c351ac955488328c6953fa0f
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2021-09-20 18:18:34 -0400

    fix spacing

--- RealmScripts/RealmController.cs
+++ RealmScripts/RealmController.cs
@@ -97,7 +97,6 @@ public class RealmController : MonoBehaviour
     public static void deleteCurrentStat()
     {
         ScoreCardManager.UnRegisterListener();
-
         realm.Write(() =>
         {
             realm.Remove(currentStat);
